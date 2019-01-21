4e6fa82707c75c483ea7143fe532501f0e86ae9e
Mitch Gavan <mitchgavan@gmail.com>
2019-01-22 07:56:39 +1100

    add api/items

---
+++ src/api/items.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    title: 'Shenmue',
+    price: 50,
+  },
+  {
+    title: 'Sonic Adventure',
+    price: 25,
+  },
+  {
+    title: 'Soul Calibur',
+    price: 30,
+  }
+]
