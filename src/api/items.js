c90e166dcdc09b3a97b43ab608ffdbed44d0e8ba
Mitch Gavan <mitchgavan@gmail.com>
2019-01-26 08:18:34 +1100

    add id to items

--- src/api/items.js
+++ src/api/items.js
@@ -1,17 +1,20 @@
 export default [
   {
+    id: 1,
     title: 'Shenmue',
     price: 50,
     category: 'Dreamcast',
   },
   {
+    id: 2,
     title: 'Sonic Adventure',
     price: 25,
     category: 'Dreamcast',
   },
   {
+    id: 3,
     title: 'Soul Calibur',
     price: 30,
     category: 'Dreamcast',
-  }
-]
+  },
+];
