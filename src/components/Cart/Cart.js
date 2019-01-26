2596ded7a62be07efc34b9a1735e11010cc07646
Mitch Gavan <mitchgavan@gmail.com>
2019-01-27 08:35:36 +1100

    feat: Cart component

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import CartItem from './CartItem/CartItem';
+import './Cart.css';
+
+export default ({ itemsInCart }) => (
+  <div className="Cart">
+    <h2 className="Cart-title">Your shopping cart</h2>
+    {itemsInCart.length ? (
+      <div>
+        {itemsInCart.map(item => (
+          <CartItem
+            key={item.id}
+            title={item.title}
+            cost={item.price * item.quantity}
+            quantity={item.quantity}
+          />
+        ))}
+        <div className="Cart-total-cost">
+          Total cost: $
+          {itemsInCart
+            .reduce((acc, item) => acc + item.price * item.quantity, 0)
+            .toFixed(2)}
+        </div>
+      </div>
+    ) : (
+      <div>Your cart is empty</div>
+    )}
+  </div>
+);
