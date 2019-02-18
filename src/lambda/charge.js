00638075cef1d8fcbf5938a2bb32d3e8a3eb8b40
Mitch Gavan <mitchgavan@gmail.com>
2019-02-18 20:55:17 +1100

    lambda function

---
+++ src/lambda/charge.js
@@ -0,0 +1,44 @@
+const stripe = require('stripe')('sk_test_yaKbjP7rkSGdMeQtQvTMx4cG');
+
+exports.handler = (event, context, callback) => {
+  // This will allow us to freeze open connections to a database
+  // context.callbackWaitsForEmptyEventLoop = false;
+
+  // Only allow POST
+  if (event.httpMethod !== 'POST') {
+    return callback(null, { statusCode: 405, body: 'Method Not Allowed' });
+  }
+
+  const data = JSON.parse(event.body);
+
+  if (!data.token || parseInt(data.amount) < 1) {
+    return callback(null, {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Some required fields were not supplied.',
+      }),
+    });
+  }
+
+  stripe.charges
+    .create({
+      amount: parseInt(data.amount),
+      currency: 'usd',
+      description: 'Dreamcast game shop',
+      source: data.token,
+    })
+    .then(({ status }) => {
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({ status }),
+      });
+    })
+    .catch(err => {
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: `Error: ${err.message}`,
+        }),
+      });
+    });
+};
