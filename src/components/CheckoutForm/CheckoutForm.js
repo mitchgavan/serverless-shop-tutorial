a148e125496a51531780bf44debe57d380a919cb
Mitch Gavan <mitchgavan@gmail.com>
2019-02-20 08:34:24 +1100

    refactor checkout form to hooks

--- src/components/CheckoutForm/CheckoutForm.js
+++ src/components/CheckoutForm/CheckoutForm.js
@@ -1,24 +1,56 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { CardElement, injectStripe } from 'react-stripe-elements';
 import './CheckoutForm.css';
 
-class CheckoutForm extends Component {
-  submit = e => {
+function CheckoutForm({ stripe }) {
+  const [status, setStatus] = useState('default');
+
+  const submit = async e => {
     e.preventDefault();
-    console.log('submitted');
+
+    setStatus('submitting');
+
+    try {
+      let { token } = await stripe.createToken({ name: 'Name' });
+
+      let response = await fetch('/.netlify/functions/charge', {
+        method: 'POST',
+        body: JSON.stringify({
+          amount: 100,
+          token: token.id,
+        }),
+      });
+
+      if (response.ok) {
+        setStatus('complete');
+      } else {
+        throw new Error('Network response was not ok.');
+      }
+    } catch (err) {
+      setStatus('error');
+    }
   };
 
-  render() {
-    return (
-      <form className="CheckoutForm" onSubmit={this.submit}>
-        <h4>Would you like to complete the purchase?</h4>
-        <CardElement />
-        <button className="CheckoutForm-button" type="submit">
-          Submit Order
-        </button>
-      </form>
-    );
+  if (status === 'complete') {
+    return <div className="CheckoutForm-complete">Payment successful!</div>;
   }
+
+  return (
+    <form className="CheckoutForm" onSubmit={submit}>
+      <h4>Would you like to complete the purchase?</h4>
+      <CardElement />
+      <button
+        className="CheckoutForm-button"
+        type="submit"
+        disabled={status === 'submitting'}
+      >
+        {status === 'submitting' ? 'Submitting' : 'Submit Order'}
+      </button>
+      {status === 'error' && (
+        <div className="CheckoutForm-error">Something went wrong.</div>
+      )}
+    </form>
+  );
 }
 
 export default injectStripe(CheckoutForm);
